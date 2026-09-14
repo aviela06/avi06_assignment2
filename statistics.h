2d6d50b8b5ed9d08a4e6c14cf394f38398ce089b
Aviela Rios Babankova <avi06@dsyslinuxp01.unm.edu>
2026-09-14 16:53:07 -0600

    adding statistics.h

--- statistics.h
+++ statistics.h
@@ -1,5 +1,11 @@
 #ifndef STATISTIC_H
 #define STATISTIC_H
 
-//define function declarations
+float average(int array[], int size);
+double standard_deviation(int array[], int size);
+int sum(int array[], int size);
+int max(int array[], int size);
+int min(int array[], int size);
+int range(int array[], int size);
+
 #endif
